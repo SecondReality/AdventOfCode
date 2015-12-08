5008a04e109d63d1b930c4380cb137b8427778d7
Steven Rose <smr@secondreality.co.uk>
2015-12-08 06:39:06 -0800

    Added QuickCheck unit test

--- day1.hs
+++ day1.hs
@@ -1,9 +1,27 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Day1 where
 import Common
+import Test.QuickCheck
 import Data.List
 
 part1 content = sum $ toNumbers content
 part2 content = elemIndex (-1) $ travel (toNumbers content)
 
 toNumbers = map (\x -> if x=='(' then 1 else -1 )
+
 travel = scanl (+) 0
+
+-- Unit Tests --
+
+newtype RandomString = RandomString String deriving (Eq,Show)
+instance Arbitrary RandomString where
+    arbitrary = sized $ \s -> do
+      n <- choose (0, s `min` 50)
+      xs <- vectorOf n (choose (')', '('))
+      return (RandomString xs)
+
+prop_part1 :: RandomString -> Bool
+prop_part1 (RandomString str) = count '(' str - count ')' str == part1 str
+
+return []
+runTests = $verboseCheckAll --quickCheckAll
