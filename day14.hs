3bbebd544e96dc9f4fc7042749e3f0431eb2de68
Steven Rose <smr@secondreality.co.uk>
2015-12-13 23:31:53 -0800

    Added day 14.

---
+++ day14.hs
@@ -0,0 +1,26 @@
+module Day14 where
+import Common
+import Text.Regex.PCRE
+import Data.List
+
+part1 content = maximum $ map (distanceTravelled 2503) (parseReindeers content)
+part2 content = maximum $ map sum $ transpose $ map (scoreAtTime content) [1..2503]
+
+distanceTravelled time (name, speed, sprintDuration, restDuration) = unclippedDistance + clippedDistance
+  where period = sprintDuration + restDuration
+        remainder = mod time period
+        unclippedDistance = speed * sprintDuration * div time period
+        clippedDistance = speed * min remainder sprintDuration
+
+type Reindeer = (String, Int, Int, Int)
+
+parseReindeers :: String -> [Reindeer]
+parseReindeers content = map parseReindeer $ lines content
+
+parseReindeer :: String -> Reindeer
+parseReindeer str = (name, read speed, read sprintDuration, read restDuration)
+  where (_,_,_, [name, speed, sprintDuration, restDuration]) = (str =~ "(\\w+) can fly (\\d+) km\\/s for (\\d+) seconds, but then must rest for (\\d+) seconds.") :: (String,String,String, [String])
+
+-- Part 2
+distancesToScore xs = map (\ x -> if x == maximum xs then 1 else 0) xs
+scoreAtTime content time = distancesToScore $ map (distanceTravelled time) (parseReindeers content)
